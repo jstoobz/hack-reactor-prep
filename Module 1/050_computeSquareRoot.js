34523201f167aebc52f0ec21b3b6f1b639cda3cf
James Stephens <stephensja7@gmail.com>
2018-07-04 14:01:50 -0400

    added hack reactor prep solution

---
+++ Module 1/050_computeSquareRoot.js
@@ -0,0 +1,13 @@
+function computeSquareRoot(num) {
+  let guess = num / 2;
+  let quotient;
+  let average;
+
+  for (var i = 0; i < 6; i++) {
+    quotient = num / guess;
+    average = (quotient + guess) / 2;
+    guess = average;
+  }
+
+  return quotient;
+}
