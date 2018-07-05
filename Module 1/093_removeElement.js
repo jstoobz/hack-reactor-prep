2d52914b416944bbfe6f55cf089a6a1f916e5b1e
James Stephens <stephensja7@gmail.com>
2018-07-05 16:19:13 -0400

    updated hack reactor prep solution

--- Module 1/093_removeElement.js
+++ Module 1/093_removeElement.js
@@ -1,6 +1,6 @@
 function removeElement(array, discarder) {
   return (!array.length) ? [] : array.reduce((acc, item) => {
-      if (item !== discarder) acc.push(item);
-      return acc;
+    if (item !== discarder) acc.push(item);
+    return acc;
   }, []);
 }
