a2126ba8ea3ec68b2af5025caded9563a8d2da0e
James Stephens <stephensja7@gmail.com>
2018-07-04 20:29:15 -0400

    updated hack reactor prep solution

--- Module 1/069_extend.js
+++ Module 1/069_extend.js
@@ -1,7 +1,5 @@
 function extend(obj1, obj2) {
-  for(let key in obj2) {
-    if(!obj1[key]) {
-      obj1[key] = obj2[key];
-    }
+  for (let key in obj2) {
+    if (!obj1[key]) obj1[key] = obj2[key];
   }
 }
