7be884590dea303c321afd1557b7eb9c337cb005
James Stephens <stephensja7@gmail.com>
2018-07-05 14:57:06 -0400

    updated hack reactor prep solution

--- Module 1/052_getLengthOfThreeWords.js
+++ Module 1/052_getLengthOfThreeWords.js
@@ -1,5 +1,5 @@
 function getLengthOfThreeWords(word1, word2, word3) {
-  return Array.from(arguments).reduce((a, b) => {
-    return a + b.length;
+  return Array.from(arguments).reduce((acc, item) => {
+    return acc + item.length;
   }, 0);
 }
