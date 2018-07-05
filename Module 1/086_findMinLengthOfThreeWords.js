f08713f31e23241fc8cdb20da340311b43788604
James Stephens <stephensja7@gmail.com>
2018-07-05 14:58:54 -0400

    updated hack reactor prep solution

--- Module 1/086_findMinLengthOfThreeWords.js
+++ Module 1/086_findMinLengthOfThreeWords.js
@@ -1,5 +1,5 @@
 function findMinLengthOfThreeWords(word1, word2, word3) {
-  return Array.from(arguments).reduce((a, b) => {
-    return Math.min(a, b.length);
+  return Array.from(arguments).reduce((acc, item) => {
+    return Math.min(acc, item.length);
   }, Infinity);
 }
