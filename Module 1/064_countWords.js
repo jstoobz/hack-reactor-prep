d037f2bd1b01d585c0c8158998b84d8dc8700c6e
James Stephens <stephensja7@gmail.com>
2018-07-05 14:58:17 -0400

    updated hack reactor prep solution

--- Module 1/064_countWords.js
+++ Module 1/064_countWords.js
@@ -1,6 +1,6 @@
 function countWords(str) {
-  return str.trim() === '' ? {} : str.split(' ').reduce((count, word) => {
-    count[word] = (count[word] || 0) + 1;
-    return count;
+  return str.trim() === '' ? {} : str.split(' ').reduce((acc, item) => {
+    acc[item] = (acc[item] || 0) + 1;
+    return acc;
   }, {});
 }
