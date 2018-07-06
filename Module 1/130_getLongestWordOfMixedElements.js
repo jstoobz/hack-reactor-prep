015c5aaecc658c9ac9821eff9f40eaf9e05e9011
James Stephens <stephensja7@gmail.com>
2018-07-06 19:02:34 -0400

    added hack reactor prep solution

---
+++ Module 1/130_getLongestWordOfMixedElements.js
@@ -0,0 +1,5 @@
+function getLongestWordOfMixedElements(arr) {
+  return (!arr.length) ? '' : arr.reduce((acc, item) => {
+    return (typeof item === 'string') && (acc === '' || item.length > acc.length) ? item : acc;
+  }, '');
+}
