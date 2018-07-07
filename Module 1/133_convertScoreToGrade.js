53cbffac3b6c22c9046ee98a382f6e3a3564a5b6
James Stephens <stephensja7@gmail.com>
2018-07-06 20:00:53 -0400

    updated hack reactor prep solution

--- Module 1/133_convertScoreToGrade.js
+++ Module 1/133_convertScoreToGrade.js
@@ -1,16 +1,10 @@
 function convertScoreToGrade(score) {
   switch(true) {
-    case score > 100 || score < 0:
-      return 'INVALID SCORE';
-    case score >= 90:
-      return 'A';
-    case score >= 80:
-      return 'B';
-    case score >= 70:
-      return 'C';
-    case score >= 60:
-      return 'D';
-    default:
-      return 'F';
+    case score > 100 || score < 0: return 'INVALID SCORE';
+    case score >= 90: return 'A';
+    case score >= 80: return 'B';
+    case score >= 70: return 'C';
+    case score >= 60: return 'D';
+    default: return 'F';
   }
 }
