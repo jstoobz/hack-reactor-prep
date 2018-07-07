3408df4f0133a00537f8854c68194a31e0634d63
James Stephens <stephensja7@gmail.com>
2018-07-07 02:49:28 -0400

    updated hack reactor prep solution

--- Module 1/123_getStringLength.js
+++ Module 1/123_getStringLength.js
@@ -1,5 +1,5 @@
 function getStringLength(string) {
-  return (string.trim() === '') ? 0 : string.split('').reduce((acc, item) => {
+  return (!string.trim()) ? 0 : string.split('').reduce((acc, item) => {
     item.slice(1);
     return count++;
   }, count = 1);
