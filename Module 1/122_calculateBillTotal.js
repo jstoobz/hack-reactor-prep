53db149f9928ba535a781c7d60dcdd68db1c18a6
James Stephens <stephensja7@gmail.com>
2018-07-07 02:48:52 -0400

    updated hack reactor prep solution

--- Module 1/122_calculateBillTotal.js
+++ Module 1/122_calculateBillTotal.js
@@ -1,6 +1,5 @@
 function calculateBillTotal(preTaxAndTipAmount) {
   let salesTax = 0.095;
   let tip = 0.15;
-
   return preTaxAndTipAmount + (preTaxAndTipAmount * salesTax) + (preTaxAndTipAmount * tip);
 }
