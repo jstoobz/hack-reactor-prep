3bff364309d30367066221ae004961afdfd76ed8
James Stephens <stephensja7@gmail.com>
2018-07-07 01:25:27 -0400

    added hack reactor prep solution

---
+++ Module 1/144_multiplyBetween.js
@@ -0,0 +1,7 @@
+function multiplyBetween(num1, num2) {
+  return (num1 > num2 || num1 === num2) ? 0 : Array.apply(null, Array(num2 - num1)).map((_, n) => {
+    return n + num1;
+  }).reduce((acc, item) => {
+    return acc * item;
+  });
+}
