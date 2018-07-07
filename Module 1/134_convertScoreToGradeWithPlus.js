eda0302723c3c561ac6118d6562b01605a212ed7
James Stephens <stephensja7@gmail.com>
2018-07-06 20:01:11 -0400

    added hack reactor prep solution

---
+++ Module 1/134_convertScoreToGradeWithPlus.js
@@ -0,0 +1,18 @@
+function convertScoreToGradeWithPlusAndMinus(score) {
+  switch(true) {
+    case score > 100 || score < 0: return 'INVALID SCORE';
+    case score >= 98: return 'A+';
+    case score >= 92: return 'A';
+    case score >= 90: return 'A-';
+    case score >= 88: return 'B+';
+    case score >= 82: return 'B';
+    case score >= 80: return 'B-';
+    case score >= 78: return 'C+';
+    case score >= 72: return 'C';
+    case score >= 70: return 'C-';
+    case score >= 68: return 'D+';
+    case score >= 62: return 'D';
+    case score >= 60: return 'D-';
+    default: return 'F';
+  }
+}
