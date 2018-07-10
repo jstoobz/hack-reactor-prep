6c5db64af03f58ac03c2f7f57d7d23262ef6c33c
James Stephens <stephensja7@gmail.com>
2018-07-10 13:06:24 -0400

    updated hack reactor prep solution

--- Module 1/096_getLastElementOfProperty.js
+++ Module 1/096_getLastElementOfProperty.js
@@ -1,3 +1,10 @@
 function getLastElementOfProperty(obj, key) {
   return (!Array.isArray(obj[key]) || !obj[key].length) ? undefined : obj[key].pop();
 }
+
+var obj = {
+  key: [1, 2, 5]
+};
+
+var output = getLastElementOfProperty(obj, 'key');
+console.log(output);
