4dc837220dedf52429c47dc7d67b522c2ab2c60c
James Stephens <stephensja7@gmail.com>
2018-07-10 13:52:13 -0400

    updated hack reactor prep solution

--- Module 1/137_getLongestOfThreeWords.js
+++ Module 1/137_getLongestOfThreeWords.js
@@ -3,3 +3,6 @@ function getLongestOfThreeWords(word1, word2, word3) {
     return acc.length >= item.length ? acc : item;
   });
 }
+
+var output = getLongestOfThreeWords('these', 'three', 'words');
+console.log(output);
