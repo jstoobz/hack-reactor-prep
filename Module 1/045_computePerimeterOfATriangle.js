4ee85905d73415edf66c1d43949d611f1773453b
James Stephens <stephensja7@gmail.com>
2018-07-10 10:38:33 -0400

    updated hack reactor prep solution

--- Module 1/045_computePerimeterOfATriangle.js
+++ Module 1/045_computePerimeterOfATriangle.js
@@ -1,5 +1,6 @@
 function computePerimeterOfATriangle(side1, side2, side3) {
-  return Array.from(arguments).reduce((acc, item) => {
-    return acc + item;
-  }, 0);
+  return Array.from(arguments).reduce((acc, item) => acc + item, 0);
 }
+
+var output = computePerimeterOfATriangle(6, 4, 10);
+console.log(output);
