73bf3e4a59259740fc7641671bb0e0b67f1c8a28
James Stephens <stephensja7@gmail.com>
2018-07-10 13:02:22 -0400

    updated hack reactor prep solution

--- Module 1/089_select.js
+++ Module 1/089_select.js
@@ -4,3 +4,14 @@ function select(arr, obj) {
     return acc;
   }, {});
 }
+
+var arr = ['a', 'c', 'e'];
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4
+};
+
+var output = select(arr, obj);
+console.log(output);
