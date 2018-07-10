8d66736cf96f434d64414c8c5ad2f57c379a051b
James Stephens <stephensja7@gmail.com>
2018-07-10 17:16:39 -0400

    updated hack reactor prep solution

--- Module 0/003_convertArrayToObject2.js
+++ Module 0/003_convertArrayToObject2.js
@@ -3,3 +3,7 @@ function fromListToObject(array) {
   array.forEach(el => obj[el[0]] = el[1]);
   return obj;
 }
+
+var arrCar = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]];
+var result = fromListToObject(arrCar);
+console.log(result);
