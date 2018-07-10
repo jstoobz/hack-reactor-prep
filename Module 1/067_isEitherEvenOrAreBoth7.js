99a5dcde319d109ad18f8e43e59e5d7fc50fa955
James Stephens <stephensja7@gmail.com>
2018-07-10 12:25:16 -0400

    updated hack reactor prep solution

--- Module 1/067_isEitherEvenOrAreBoth7.js
+++ Module 1/067_isEitherEvenOrAreBoth7.js
@@ -1,3 +1,9 @@
 function isEitherEvenOrAreBoth7(num1, num2) {
-  return (num1 % 2 === 0 || num2 % 2 === 0 || num1 === 7 && num2 === 7);
+  return (num1 % 2 === 0 || num2 % 2 === 0 || (num1 === 7 && num2 === 7));
 }
+
+var output = isEitherEvenOrAreBoth7(3, 7);
+console.log(output);
+
+var output = isEitherEvenOrAreBoth7(2, 3);
+console.log(output);
