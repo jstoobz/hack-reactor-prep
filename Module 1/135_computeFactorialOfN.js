3719eb344f9f9cc2e2a8315f2f7a5b36c0c9f8d7
James Stephens <stephensja7@gmail.com>
2018-07-10 13:51:08 -0400

    updated hack reactor prep solution

--- Module 1/135_computeFactorialOfN.js
+++ Module 1/135_computeFactorialOfN.js
@@ -1,3 +1,9 @@
 function computeFactorialOfN(n) {
   return (n === 0) ? 1 : n * computeFactorialOfN(n - 1);
 }
+
+var output = computeFactorialOfN(3);
+console.log(output);
+
+var output = computeFactorialOfN(4);
+console.log(output);
