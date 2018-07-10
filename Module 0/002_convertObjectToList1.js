86533ce507172601b94b05c2fe9049a8eda856ff
James Stephens <stephensja7@gmail.com>
2018-07-10 17:15:35 -0400

    updated hack reactor prep solution

--- Module 0/002_convertObjectToList1.js
+++ Module 0/002_convertObjectToList1.js
@@ -3,3 +3,12 @@ function getAllKeys(obj){
   for (let key in obj) keys.push(key);
   return keys;
 }
+
+var person = {
+  name : 'Sam',
+  age : 25,
+  hasPets : true
+};
+
+var result = getAllKeys(person);
+console.log(result);
