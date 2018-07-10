e7e942a6308807f8c22a87f583689aec815bc268
James Stephens <stephensja7@gmail.com>
2018-07-10 13:08:09 -0400

    updated hack reactor prep solution

--- Module 1/099_computeAverageOfNumbers.js
+++ Module 1/099_computeAverageOfNumbers.js
@@ -3,3 +3,7 @@ function computeAverageOfNumbers(nums) {
     return acc + item;
   }, 0) / nums.length;
 }
+
+var input = [1,2,3,4,5];
+var output = computeAverageOfNumbers(input);
+console.log(output);
