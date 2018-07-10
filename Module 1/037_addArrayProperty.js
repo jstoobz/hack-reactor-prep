a89cdf36a7a1aae1487cbe44a4aaad75c2ddd8cf
James Stephens <stephensja7@gmail.com>
2018-07-10 10:07:47 -0400

    updated hack reactor prep solution

--- Module 1/037_addArrayProperty.js
+++ Module 1/037_addArrayProperty.js
@@ -1,3 +1,9 @@
 function addArrayProperty(obj, key, arr) {
   return obj[key] = arr;
 }
+
+var myObj = {};
+var myArray = [1, 3];
+
+addArrayProperty(myObj, 'myProperty', myArray);
+console.log(myObj.myProperty);
