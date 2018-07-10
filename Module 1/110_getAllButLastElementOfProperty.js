2a22d97a2ee8c5a42b8437e50f8555d479924748
James Stephens <stephensja7@gmail.com>
2018-07-10 13:19:28 -0400

    updated hack reactor prep solution

--- Module 1/110_getAllButLastElementOfProperty.js
+++ Module 1/110_getAllButLastElementOfProperty.js
@@ -1,3 +1,10 @@
 function getAllButLastElementOfProperty(obj, key) {
   return (!Array.isArray(obj[key]) || !obj[key].length) ? [] : obj[key].slice(0, -1);
 }
+
+var obj = {
+  key: [1, 2, 3]
+};
+
+var output = getAllButLastElementOfProperty(obj, 'key');
+console.log(output);
