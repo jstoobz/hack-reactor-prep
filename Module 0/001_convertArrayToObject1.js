3ff6a28393a95b259f2cd1d8826f6e976c98c323
James Stephens <stephensja7@gmail.com>
2018-07-10 14:01:02 -0400

    updated hack reactor prep solution

--- Module 0/001_convertArrayToObject1.js
+++ Module 0/001_convertArrayToObject1.js
@@ -1,3 +1,7 @@
 function transformFirstAndLast(array) {
-  return {[array[0]]: array[array.length - 1]};
+  return { [array[0]]: array[array.length - 1] };
 }
+
+var myArr = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
+var result = transformFirstAndLast(myArr);
+console.log(result);
