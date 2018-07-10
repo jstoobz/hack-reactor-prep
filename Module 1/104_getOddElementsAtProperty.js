8ee40dcad374646fbf90ed5a98e53ac3b0c86f36
James Stephens <stephensja7@gmail.com>
2018-07-10 13:13:55 -0400

    updated hack reactor prep solution

--- Module 1/104_getOddElementsAtProperty.js
+++ Module 1/104_getOddElementsAtProperty.js
@@ -3,3 +3,10 @@ function getOddElementsAtProperty(obj, key) {
     if (el % 2 !== 0) return [].push(el);
   }, []);
 }
+
+var obj = {
+  key: [1, 2, 3, 4, 5]
+};
+
+var output = getOddElementsAtProperty(obj, 'key');
+console.log(output);
