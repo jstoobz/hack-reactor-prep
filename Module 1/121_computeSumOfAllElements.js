8799b8fa9a2e07c08fe63337bfb81c35388f5c79
James Stephens <stephensja7@gmail.com>
2018-07-10 13:28:46 -0400

    updated hack reactor prep solution

--- Module 1/121_computeSumOfAllElements.js
+++ Module 1/121_computeSumOfAllElements.js
@@ -1,5 +1,6 @@
 function computeSumOfAllElements(arr) {
-  return arr.reduce((acc, item) => {
-    return acc + item;
-  }, 0);
+  return arr.reduce((acc, item) => acc + item, 0);
 }
+
+var output = computeSumOfAllElements([1, 2, 3])
+console.log(output);
