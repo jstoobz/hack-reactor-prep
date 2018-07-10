563c213da24f2cdfa19bf03feb8dba282130a508
James Stephens <stephensja7@gmail.com>
2018-07-10 13:34:08 -0400

    updated hack reactor prep solution

--- Module 1/125_getProductOfAllElementsAtProperty.js
+++ Module 1/125_getProductOfAllElementsAtProperty.js
@@ -1,5 +1,10 @@
 function getProductOfAllElementsAtProperty(obj, key) {
-  return (!Array.isArray(obj[key]) || !obj[key].length) ? 0 : obj[key].reduce((acc, item) => {
-    return acc * item;
-  }, 1);
+  return (!Array.isArray(obj[key]) || !obj[key].length) ? 0 : obj[key].reduce((acc, item) => acc * item, 1);
 }
+
+var obj = {
+  key: [1, 2, 3, 4]
+};
+
+var output = getProductOfAllElementsAtProperty(obj, 'key');
+console.log(output);
