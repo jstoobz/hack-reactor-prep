6b4f1c9a8f09258195696951360ced5d25f577a5
James Stephens <stephensja7@gmail.com>
2018-07-10 10:04:45 -0400

    updated hack reactor prep solution

--- Module 1/031_addFullNameProperty.js
+++ Module 1/031_addFullNameProperty.js
@@ -1,3 +1,11 @@
 function addFullNameProperty(obj) {
   return obj['fullName'] = obj.firstName.concat(` ${obj.lastName}`);
 }
+
+var person = {
+  firstName: 'Jade',
+  lastName: 'Smith'
+};
+
+addFullNameProperty(person);
+console.log(person.fullName);
