c10e0a120abeb0a41d4d48e8ccf87802be77dd6e
James Stephens <stephensja7@gmail.com>
2018-07-10 13:19:59 -0400

    updated hack reactor prep solution

--- Module 1/111_getElementOfArrayProperty.js
+++ Module 1/111_getElementOfArrayProperty.js
@@ -1,3 +1,10 @@
 function getElementOfArrayProperty(obj, key, index) {
   return (!Array.isArray(obj[key]) || !obj[key].length) ? undefined : obj[key][index];
 }
+
+var obj = {
+ key: ['Jamil', 'Albrey']
+};
+
+var output = getElementOfArrayProperty(obj, 'key', 0);
+console.log(output);
