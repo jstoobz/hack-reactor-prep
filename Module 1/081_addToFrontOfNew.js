a1e3991ce03de735cc4242ef9bc3afc79411aa56
James Stephens <stephensja7@gmail.com>
2018-07-10 12:36:21 -0400

    updated hack reactor prep solution

--- Module 1/081_addToFrontOfNew.js
+++ Module 1/081_addToFrontOfNew.js
@@ -1,3 +1,8 @@
 function addToFrontOfNew(arr, element) {
   return [element].concat(arr);
 }
+
+var input = [1, 2];
+var output = addToFrontOfNew(input, 3);
+console.log(output);
+console.log(input);
