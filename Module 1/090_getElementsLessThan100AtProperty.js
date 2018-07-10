7ae27fecf631271e0ef145ea0618a5a6ad918150
James Stephens <stephensja7@gmail.com>
2018-07-10 13:02:51 -0400

    updated hack reactor prep solution

--- Module 1/090_getElementsLessThan100AtProperty.js
+++ Module 1/090_getElementsLessThan100AtProperty.js
@@ -3,3 +3,10 @@ function getElementsLessThan100AtProperty(obj, key) {
     if (el < 100) return [].push(el);
   }, []);
 }
+
+var obj = {
+  key: [1000, 20, 50, 500]
+};
+
+var output = getElementsLessThan100AtProperty(obj, 'key');
+console.log(output);
