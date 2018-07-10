9966e5e0120cdb8e9479772a8daaf0b74b0c5034
James Stephens <stephensja7@gmail.com>
2018-07-10 13:53:58 -0400

    updated hack reactor prep solution

--- Module 1/139_computeCompoundInterest.js
+++ Module 1/139_computeCompoundInterest.js
@@ -1,3 +1,6 @@
 function computeCompoundInterest(principal, interestRate, compoundingFrequency, timeInYears) {
-  return principal * Math.pow((1 + interestRate / compoundingFrequency), (compoundingFrequency * timeInYears)) - principal;
+  return principal * ((1 + interestRate / compoundingFrequency) ** (compoundingFrequency * timeInYears)) - principal;
 }
+
+var output = computeCompoundInterest(1500, .043, 4, 6);
+console.log(output);
