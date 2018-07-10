01ad2465176e008f08f986b074812f2f1b0a4502
James Stephens <stephensja7@gmail.com>
2018-07-10 13:06:03 -0400

    updated hack reactor prep solution

--- Module 1/095_getNthElementOfProperty.js
+++ Module 1/095_getNthElementOfProperty.js
@@ -1,3 +1,10 @@
 function getNthElementOfProperty(obj, key, n) {
   return (!Array.isArray(obj[key]) || !obj[key].length) ? undefined : obj[key][n];
 }
+
+var obj = {
+  key: [1, 2, 6]
+};
+
+var output = getNthElementOfProperty(obj, 'key', 1);
+console.log(output);
