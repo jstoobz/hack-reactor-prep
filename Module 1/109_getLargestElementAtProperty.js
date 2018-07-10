64b6f9013c36562cbd29a41e914d9e1970f56263
James Stephens <stephensja7@gmail.com>
2018-07-10 13:18:23 -0400

    updated hack reactor prep solution

--- Module 1/109_getLargestElementAtProperty.js
+++ Module 1/109_getLargestElementAtProperty.js
@@ -1,5 +1,10 @@
 function getLargestElementAtProperty(obj, key) {
-  return (!Array.isArray(obj[key]) || !obj[key].length) ? undefined : obj[key].reduce((acc, item) => {
-    return Math.max(acc, item);
-  }, -Infinity);
+  return (!Array.isArray(obj[key]) || !obj[key].length) ? undefined : Math.max(...obj[key]);
 }
+
+var obj = {
+  key: [1, 2, 4]
+};
+
+var output = getLargestElementAtProperty(obj, 'key');
+console.log(output);
