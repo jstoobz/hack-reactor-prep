50af4aeb90a42aa39be8ce9c81ba6be674ed7a5c
James Stephens <stephensja7@gmail.com>
2018-07-10 17:22:12 -0400

    updated hack reactor prep solution

--- Module 0/004_convertObjectToArray2.js
+++ Module 0/004_convertObjectToArray2.js
@@ -3,3 +3,12 @@ function listAllValues(obj) {
   for (let value in obj) values.push(obj[value]);
   return values;
 }
+
+person = {
+  name : 'Krysten',
+  age : 33,
+  hasPets : false
+};
+
+var result = listAllValues(person);
+console.log(result);
