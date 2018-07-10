0f117fd3efdc01c4be72121caa0a9a06267bb661
James Stephens <stephensja7@gmail.com>
2018-07-10 12:39:28 -0400

    updated hack reactor prep solution

--- Module 1/084_areValidCredentials.js
+++ Module 1/084_areValidCredentials.js
@@ -1,3 +1,6 @@
 function areValidCredentials(name, password) {
   return (name.length > 3 && password.length >= 8);
 }
+
+var output = areValidCredentials('Ritu', 'mylongpassword')
+console.log(output);
