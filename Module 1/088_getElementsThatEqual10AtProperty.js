76a0c0d0b18c2b6ae4426dbacf63d3693c21e3ea
James Stephens <stephensja7@gmail.com>
2018-07-10 13:01:18 -0400

    updated hack reactor prep solution

--- Module 1/088_getElementsThatEqual10AtProperty.js
+++ Module 1/088_getElementsThatEqual10AtProperty.js
@@ -3,3 +3,10 @@ function getElementsThatEqual10AtProperty(obj, key) {
     if (el === 10) return [].push(el);
   }, []);
 }
+
+var obj = {
+  key: [1000, 10, 50, 10]
+};
+
+var output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output);
