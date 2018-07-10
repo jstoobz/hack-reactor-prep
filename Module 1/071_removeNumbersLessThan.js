740d503b3ce2943adab0965cfb8dbc5de157406b
James Stephens <stephensja7@gmail.com>
2018-07-10 12:29:17 -0400

    updated hack reactor prep solution

--- Module 1/071_removeNumbersLessThan.js
+++ Module 1/071_removeNumbersLessThan.js
@@ -3,3 +3,12 @@ function removeNumbersLessThan(num, obj) {
     if (obj[key] < num) delete obj[key];
   });
 }
+
+var obj = {
+  a: 8,
+  b: 2,
+  c: 'montana'
+};
+
+removeNumbersLessThan(5, obj);
+console.log(obj);
