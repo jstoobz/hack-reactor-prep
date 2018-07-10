b8a18798b6408322e6c6100b0a31dddfa6e33e27
James Stephens <stephensja7@gmail.com>
2018-07-10 13:04:00 -0400

    updated hack reactor prep solution

--- Module 1/092_getElementsGreaterThan10AtProperty.js
+++ Module 1/092_getElementsGreaterThan10AtProperty.js
@@ -3,3 +3,10 @@ function getElementsGreaterThan10AtProperty(obj, key) {
     if (el > 10) return [].push(el);
   }, []);
 }
+
+var obj = {
+  key: [1, 20, 30]
+};
+
+var output = getElementsGreaterThan10AtProperty(obj, 'key');
+console.log(output);
