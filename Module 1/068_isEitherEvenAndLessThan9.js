6bba06dc783345d60017085ca2e7e26973f4bb2e
James Stephens <stephensja7@gmail.com>
2018-07-10 12:26:58 -0400

    updated hack reactor prep solution

--- Module 1/068_isEitherEvenAndLessThan9.js
+++ Module 1/068_isEitherEvenAndLessThan9.js
@@ -1,3 +1,9 @@
 function isEitherEvenAndLessThan9(num1, num2) {
-  return (num1 % 2 === 0 || num2 % 2 === 0) && (num1 < 9 && num2 < 9);
+  return ((num1 % 2 === 0 || num2 % 2 === 0) && (num1 < 9 && num2 < 9));
 }
+
+var output = isEitherEvenAndLessThan9(2, 4);
+console.log(output);
+
+var output = isEitherEvenAndLessThan9(72, 2);
+console.log(output);
