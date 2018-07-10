3d9464886908ab9d8662e590dc71eafd0ca6df50
James Stephens <stephensja7@gmail.com>
2018-07-10 10:36:41 -0400

    updated hack reactor prep solution

--- Module 1/044_computePerimeterOfARectangle.js
+++ Module 1/044_computePerimeterOfARectangle.js
@@ -1,3 +1,6 @@
 function computePerimeterOfARectangle(length, width) {
-  return (length * 2) + (width * 2);
+  return (length + width) * 2;
 }
+
+var output = computePerimeterOfARectangle(5, 2);
+console.log(output);
