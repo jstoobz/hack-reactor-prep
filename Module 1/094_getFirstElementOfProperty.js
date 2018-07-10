67bb9ada46d25cbecc88a150023cb6a8aba3c273
James Stephens <stephensja7@gmail.com>
2018-07-10 13:05:39 -0400

    updated hack reactor prep solution

--- Module 1/094_getFirstElementOfProperty.js
+++ Module 1/094_getFirstElementOfProperty.js
@@ -1,3 +1,6 @@
 function getFirstElementOfProperty(obj, key) {
   return (!Array.isArray(obj[key]) || !obj[key].length) ? undefined : obj[key][0];
 }
+
+var output = getFirstElementOfProperty(obj, 'key');
+console.log(output);
