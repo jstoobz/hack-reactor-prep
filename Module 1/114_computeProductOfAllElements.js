3b7d1638d27862a3664db5c2c0f99a6b281ac92d
James Stephens <stephensja7@gmail.com>
2018-07-10 13:24:03 -0400

    updated hack reactor prep solution

--- Module 1/114_computeProductOfAllElements.js
+++ Module 1/114_computeProductOfAllElements.js
@@ -1,5 +1,6 @@
 function computeProductOfAllElements(arr) {
-  return (!arr.length) ? 0 : arr.reduce((acc, item) => {
-    return acc * item;
-  }, 1);
+  return (!arr.length) ? 0 : arr.reduce((acc, item) => acc * item, 1);
 }
+
+var output = computeProductOfAllElements([2, 5, 6]);
+console.log(output);
